da34a80ecba3a4b8c00924e613b240ab08a8cb2d
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-09-09 13:04:41 +0100

    remove old containers before tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo docker build -f tests/Dockerfile -t dropwiz .
-sudo docker run -p 9080:8080 -p 9081:8081 dropwiz
+CONTAINERS=$(docker ps -q -a)
+if [ "$CONTAINERS" ]
+then
+	docker stop $CONTAINERS &> /dev/null
+	docker rm $CONTAINERS &> /dev/null
+fi
+
+docker build -f tests/Dockerfile -t dropwiz .
+docker run -p 9080:8080 -p 9081:8081 dropwiz
